ec6c7325d774da074bb65c07d8bbe26838266824
Matheus Omena <omena.theus@live.com>
2022-06-13 17:26:37 -0300

    ajuste nas dimensões dos containers

--- web-ui/tailwind.config.js
+++ web-ui/tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   theme: {
     extend: {},
   },
+  corePlugins: {
+    container: false
+  },
   plugins: [
     require('@tailwindcss/forms'),
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1320px',
+          },
+        }
+      })
+    }
   ],
 }
